c7c78090d70227768bb1e321d0a8adf41a918131
Niyas Sait <niyas.sait@linaro.org>
2022-02-09 18:02:10 +0000

    add appveyor scripts

---
+++ build.ps1
@@ -0,0 +1,11 @@
+
+$vcvarsarch = "x86_amd64"
+
+cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars$vcvarsarch.bat`" && set > %temp%\vcvars$vcvarsarch.txt"
+Get-Content "$env:temp\vcvars$vcvarsarch.txt" | Foreach-Object {
+    if ($_ -match "^(.*?)=(.*)$") {
+        Set-Content "env:\$($matches[1])" $matches[2]
+    }
+}
+
+Start-Process -NoNewWindow -Wait nmake "-f win/makefile.vc OPTS=static all"
