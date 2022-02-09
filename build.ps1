608596c021205fd9451d91f5b0cba6e6573d503b
Niyas Sait <niyas.sait@linaro.org>
2022-02-09 19:31:09 +0000

    add appveyor scripts

--- build.ps1
+++ build.ps1
@@ -1,20 +1,34 @@
+<#
+This script builds libiconv,libxml2 and libxslt
+#>
+Param(
+    [switch]$x64,
+    [switch]$arm64,
+    [switch]$vs2008
+)
+
 $ErrorActionPreference = "Stop"
 Import-Module Pscx
 
-$vcvarsarch = "x86_arm64"
+$platDir = If($x64) { "\x64" } ElseIf ($arm64) { "\arm64" } Else { "" }
+$distname = If($x64) { "win64" } ElseIf($arm64) { "win-arm64" } Else { "win32" }
+If($vs2008) { $distname = "vs2008.$distname" }
 
-cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars$vcvarsarch.bat`" && set > %temp%\vcvars$vcvarsarch.txt"
-Get-Content "$env:temp\vcvars$vcvarsarch.txt" | Foreach-Object {
-    if ($_ -match "^(.*?)=(.*)$") {
-        Set-Content "env:\$($matches[1])" $matches[2]
+If($vs2008) {
+    $vcvarsarch = If($x64) { "amd64" } Else { "x86" }
+    Import-VisualStudioVars -VisualStudioVersion "90" -Architecture $vcvarsarch
+} Else {
+    $vcvarsarch = If($x64) { "x86_amd64" } ElseIf ($arm64) { "x86_arm64" } Else { "32" }
+    cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars$vcvarsarch.bat`" && set > %temp%\vcvars$vcvarsarch.txt"
+    Get-Content "$env:temp\vcvars$vcvarsarch.txt" | Foreach-Object {
+        if ($_ -match "^(.*?)=(.*)$") {
+            Set-Content "env:\$($matches[1])" $matches[2]
+        }
     }
 }
 
-cd win
+Set-Location $PSScriptRoot
 
+Set-Location .\win
 
-Start-Process -NoNewWindow -Wait nmake "-f makefile.vc OPTS=static MACHINE=ARM64 all"
-
-Start-Process -NoNewWindow -Wait nmake "-f makefile.vc OPTS=static MACHINE=ARM64 all"
-
-New-Item -ItemType Directory .\dist
+Start-Process -NoNewWindow -Wait nmake "-f makefile.vc all"
